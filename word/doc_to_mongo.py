e1d37ef4f0553866757ef53824e58d9cc407c46c
dmquant <liudaoming@vip.163.com>
2014-07-27 07:02:46 +0800

    modify word/doc_to_mongo.py

--- word/doc_to_mongo.py
+++ word/doc_to_mongo.py
@@ -19,7 +19,7 @@ from os.path import exists
 # 7. Move file to dest directory
 # 8. Save data to mongo
 
-MONGO_CLIENT = MongoClient(‘172.20.2.207’)
+MONGO_CLIENT = MongoClient('172.20.2.207')
 MONGO_WORD_DB = MONGO_CLIENT['word']
 MONGO_WORD_COLLECTION = MONGO_WORD_DB['keywords']
 MONGO_DOC_DB = MONGO_CLIENT['doc']
@@ -33,20 +33,19 @@ XML_DEST_DIR = '/home/daoming/xml/'
 def jiebaToList(data):
     wordList = []
     for tk in data:
-        wordList.append({'word':tk[0].decode('gbk'),'pos':tk[1]})
+        wordList.append({'word':tk[0],'pos':tk[1]})
     return wordList
 
 jieba.initialize()
 jieba.set_dictionary('dict.txt.big')
-reader = MONG_WORD_COLLECTION.find({'user':1,'cut':1})
+reader = MONGO_WORD_COLLECTION.find({'user':1,'cut':1})
 for word in reader:
     jieba.add_word(word['name'],word['weight'],word['tag'])
-
 dictConceptList = {}
 reader = MONGO_CONCEPT_COLLECTION.find()
 for word in reader:
     jieba.add_word(word['conceptname'],999,'NCI')
-    dictConceptList[word['conceptname'].decode('gbk')] = word['conceptid']
+    dictConceptList[word['conceptname']] = word['conceptid']
 
 filenameList=glob.glob(XML_SOURCE_FILE)
 fileList = []
@@ -63,41 +62,39 @@ for filename in filenameList:
             filedate = xmldict['document']['header']['timestamp'][0:8]
             article = xmldict['document']['articles']['article']
             titledata = xmldict['document']['basicinfo']['title']
-            postdata = articles['topicinfo']['postdata']
-            cutTitle = jieba.tokenize(titledata,mode='default')
-            cutData = jieba.tokenize(postdata,mode='default')
+            postdata = article['topicinfo']['postdata']
+            dictCutTitle = jiebaToList(jieba.tokenize(titledata,mode='default'))
+            dictCutData = jiebaToList(jieba.tokenize(postdata,mode='default'))
             posttime = xmldict['document']['articles']['article']['topicinfo']['posttime']
-            tmpDir = XML_DEST_FILE + filedate
-            cutResult = {'result':'1','posttime':posttime,'cuttitle':cutTitle,'cutdata':cutData}
-            dictCutData = jiebaToList(cutData)
-            dictCutTitle = jiebaToList(cutTitle)
+            tmpDir = XML_DEST_DIR + filedate
+            cutResult = {'result':'1','posttime':posttime,'cuttitle':dictCutTitle,'cutdata':dictCutData}
             dictConceptStat = {}
             listConceptWord = []
             for word in dictCutTitle:
                 if dictConceptStat.has_key(word['word']):
-                    dictConceptStat[word['word']] = dictConceptStat[word['word']] + 2
+                    	dictConceptStat[word['word']] = dictConceptStat[word['word']] + 2
                 else:
-                    if dictConceptList.has_key(word['word']):
-                        dictConceptStat[word['word']] = 2
+                    	if dictConceptList.has_key(word['word']):
+                        	dictConceptStat[word['word']] = 2
             
             for word in dictCutData:
                 if dictConceptStat.has_key(word['word']):
-                    dictConceptStat[word['word']] = dictConceptStat[word['word']] + 1
+                    	dictConceptStat[word['word']] = dictConceptStat[word['word']] + 1
                 else:
-                    if dictConceptList.has_key(word['word']):
-                        dictConceptStat[word['word']] = 1
+                    	if dictConceptList.has_key(word['word']):
+                        	dictConceptStat[word['word']] = 1
             if len(dictConceptStat) > 0 and len(dictConceptStat) < 4:
                 s = float(sum(dictConceptStat.values()))
                 for word,value in sorted(dictConceptStat.items(),key=lambda d:d[1],reverse=True):
-                    listConceptWord.append('conceptname':word,'conceptid':dictConceptList[word],'corr':float(value)/s)
+                    	listConceptWord.append({'conceptname':word,'conceptid':dictConceptList[word],'corr':float(value)/s})
                 cutResult['concept'] = '1'
+		print "========================================================"
                 cutResult['conceptlist'] = []
                 for concept in listConceptWord:
-                    cutResult['conceptlist'].append({'conceptid':concept['conceptid'],'id':concept['conceptid'],'name':concept['conceptname'],'conceptname':concept['conceptname'],'corr':'%.2f' % concept['corr']})
-            
-        except:
-            tmpDir = XML_DEST_FILE + 'error/' + filedate
-            cutResult = {'result':'0'}
+                    	cutResult['conceptlist'].append({'conceptid':concept['conceptid'],'id':concept['conceptid'],'name':concept['conceptname'],'conceptname':concept['conceptname'],'corr':'%.2f' % concept['corr']})
+        except Exception as err:
+            tmpDir = XML_DEST_DIR + 'error/' + filedate
+            cutResult = {'result':'0','posttime':0} 
         i = i + 1
         if i % 1000 == 0:
             print i
@@ -108,10 +105,11 @@ for filename in filenameList:
     
 #    os.rename(filename,destFile)
 
-fileList = sorted(fileList,key=lambda files:(files['posttime']),reverse=True)
+fileList = sorted(fileList,key=lambda files:(files['cutresult']['posttime']),reverse=True)
 for doc in fileList:
     try:
         os.rename(doc['srcfile'],doc['destfile'])
-        doc_collection.insert(doc)
-    except:
-        continue
+	print doc['destfile']
+        MONGO_DOC_COLLECTION.insert(doc)
+    except Exception as err:
+	continue
